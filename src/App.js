251379fc3b10d87ccb527a50281ec10858fb2a99
KFChinese <larrychiem@gmail.com>
2022-06-07 22:58:51 -0700

    Changes to Login to include Oauth2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import SchedList from './components/SchedList';
 import Semester from './components/Semester';
+import Login from './components/Login';
 import {BrowserRouter, Switch, Route, Link} from 'react-router-dom';
 import AddStudent from './components/AddStudent';
 import Button from '@mui/material/Button';
@@ -26,11 +27,18 @@ function App() {
         <Button component={Link} to="/AddStudent" variant="outlined" color="primary" > 
           Add Student
           </Button>
-          
+          <Button component={Link} to="/login" variant="outlined" color="primary" > 
+          login
+          </Button>
           <Semester />
         </Route>
+        <BrowserRouter>
+       <Switch>
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/semester' component={Semester} />
         <Route path='/schedule' component={SchedList} />
-        <Route path='/AddStudent' component={AddStudent} />
+       </Switch>
+      </BrowserRouter>
        </Switch>
       </BrowserRouter>
     </div>
